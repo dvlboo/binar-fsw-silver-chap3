e06f03dc1e106c397523c9e5cf46d561da08081b
dvlboo <kuhhhckr@gmail.com>
2024-03-13 23:38:38 +0700

    update : method filter student

--- topic-1/index.js
+++ topic-1/index.js
@@ -1,4 +1,4 @@
-const fs = require('fs')
+// const fs = require('fs')
 const express = require('express')
 
 
@@ -8,23 +8,72 @@ const student = require('./data/students.json')
 
 app.use(express.static('public'))
 
+
+// by query
 app.get('/student', (req, res) => {
-  // Write File
-  // fs.writeFileSync('./data/write.json', `{"success" : true}`, 'utf-8')
   
   const {name, city, province} = req.query
   // console.log(req.query.province)
   let dataStudent = [...student]
   // console.log(dataStudent)
+  
+  dataStudent = dataStudent.filter((student) => {
+    let filteredStatus = true
 
-  dataStudent = dataStudent.filter(
-    (student) => 
-      student.name.toLowerCase().includes(name.toLowerCase()) &&
-      student.address.city.toLowerCase().includes(city.toLowerCase()) &&
-      student.address.province.toLowerCase().includes(province.toLowerCase())
-  )
+    if (name) {
+      filteredStatus = 
+        filteredStatus && student.name
+          .toLowerCase()
+          .includes(name?.toLowerCase()) //tanda "?" => untuk skipping data jika undefine
+    }
 
-  res.status(200).json(dataStudent)
+    if (city) {
+      filteredStatus = 
+        filteredStatus && student.address.city
+          .toLowerCase()
+          .includes(city?.toLowerCase())
+    }
+
+    if (province) {
+      filteredStatus = 
+        filteredStatus && student.address.province
+          .toLowerCase()
+          .includes(province?.toLowerCase())
+    }
+
+    return filteredStatus
+  
+  })
+
+  // standart handling data
+  const response = {
+    dataStudent,
+    message: null,
+  }
+  
+  res.status(200).json(response)
+})
+
+
+// search by params
+app.get('/students/:id', (req, res) => {
+  // console.log(req.params)
+  const { id } = req.params
+
+  let dataStudent = [...student]
+
+  dataStudent = dataStudent.filter(student => student.id == id)
+  
+  // standart handling data
+  const response = {
+    data: dataStudent[0],
+    message: null,
+  }
+
+  dataStudent.length == 0 
+    ? res.status(404).json({message : `Student with id ${id} is Not Found!`}) 
+    : res.status(200).json(response)
+  console.log(response)
 })
 
 app.listen(port, () => {
