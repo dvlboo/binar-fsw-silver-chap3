f0769c62dfc766be9c95721f549ae6d9fc4d456e
dvlboo <kuhhhckr@gmail.com>
2024-03-17 15:22:10 +0700

    fixed

--- topic-1/index.js
+++ topic-1/index.js
@@ -12,21 +12,6 @@ app.use(express.json())
 app.use(express.static('public'))
 app.use("/", route)
 
-// app.delete("/students/:id", (req, res) => {
-//   const id = parseInt(req?.params?.id)
-
-//   const index = student.findIndex(student => student.id === id);
-//   if (index === -1) {
-//     return res.status(404).json({ message: `Student with id ${id} is Not Found!` });
-//   }
-//   student.splice(index, 1);
-
-//   const response = {
-//     message : "Student data deleted successfully"
-//   }
-//   res.status(200).json(response)
-// })
-
 app.listen(port, () => {
   console.log(`Listening server on port : ${port}`)
 })
